88f6ecd4c92befe20c2e58c5b7b185994851e297
luigimapelli8-cyber <luigimapelli8@gmail.com>
2026-08-14 22:56:14 +0200

    Remove Netlify configuration, restore Replit deployment

--- netlify/functions/api.ts
+++
@@ -1,7 +0,0 @@
-// @ts-ignore - serverless-http will be installed during Netlify build
-import serverless from 'serverless-http';
-// @ts-ignore - app will be available after build
-import app from '../../artifacts/api-server/src/app.js';
-
-// Netlify serverless function handler using serverless-http
-export const handler = serverless(app);
